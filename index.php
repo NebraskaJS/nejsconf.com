4ec46d1e31b2706f012e28712af7d52c06a00b85
John Hobbs <john@velvetcache.org>
2015-02-10 00:38:59 -0600

    Clean up PHP, show and hide buttons and form as needed.

--- index.php
+++ index.php
@@ -4,55 +4,60 @@ layout: default
 
 {% raw %}
 <?php
-require 'vendor/autoload.php';
-$config = require 'config.php';
+  require 'vendor/autoload.php';
+  $config = require 'config.php';
 
-$show_form = true;
+  $render_form = true;
+  $render_success = false;
+  $form_collapsed = true;
+  $error = null;
 
-if( $_POST ) {
-  $mc = new Mailchimp($config['api-key']);
-  curl_setopt($mc->ch, CURLOPT_SSL_VERIFYHOST, 0);
-  curl_setopt($mc->ch, CURLOPT_SSL_VERIFYPEER, 0);
+  if( $_POST ) {
+    $form_collapsed = false;
 
-  try {
-    $mc->lists->subscribe($config['list-id'],
-                          array('email' => $_POST['email']),
-                          null,
-                          'html',
-                          false,
-                          true,
-                          true,
-                          true);
-    $show_form = false;
-?>
-<div class="form-result success" id="feedback">Awesome! We will be in touch!</div>
-<?php if(isset($_POST['email'])): ?><div class="form-result feedback"><?php echo htmlspecialchars($_POST['email']); ?></div><?php endif; ?>
-<?php
-  } catch (Mailchimp_Error $e) {
-    if (empty($_POST['email'])) {
-?>
-<div class="form-result error" id="feedback">An email is required.</div>
-<?php
-    } elseif ($e->getMessage()) {
-?>
-<div class="form-result error" id="feedback"><?php echo $e->getMessage(); ?></div>
-<?php
-    } else {
-?>
-<div class="form-result error" id="feedback">Could not subscribe. Please try again.</div>
-<?php
+    $mc = new Mailchimp($config['api-key']);
+    curl_setopt($mc->ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($mc->ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+    try {
+      $mc->lists->subscribe($config['list-id'], 
+                            array('email' => $_POST['email']),
+                            null,
+                            'html',
+                            false, 
+                            true,
+                            true,
+                            true);
+      $render_form = false;
+      $render_success = true;
+    } 
+    catch (Mailchimp_Error $e) {
+      if (empty($_POST['email'])) { 
+        $error = "An email is required.";
+      }
+      else {
+        $error = $e->getMessage();
+        if ( ! $error) {
+          $error = 'Could not subscribe. Please try again.';
+        }
+      }
     }
   }
-}
+?>
 
-if( $show_form ): ?>
+<?php if( $render_success ): ?>
+  <div class="form-result success" id="feedback">Awesome! We will be in touch!</div>
+  <div class="form-result feedback"><?php echo htmlspecialchars($_POST['email']); ?></div>
+<?php endif; ?>
+<?php if( $error ): ?><div class="form-result error" id="feedback"><?php echo htmlspecialchars($error); ?></div><?php endif; ?>
+
+<?php if( $render_form ): ?>
 <form method="POST" action="<?php echo $PHP_SELF; ?>#feedback" id="teaser-subscribe"  class="collapsed">
   <label for="teaser-email" class="a11y-only">Email</label>
   <input type="text" id="teaser-email" name="email" <?php if(isset($_POST['email'])): ?>value="<?php echo htmlspecialchars($_POST['email']); ?>"<?php endif; ?> placeholder="Your e-mail address">
   <button type="submit" class="btn-primary">Get notified <span>when tickets go on sale</span></button>
 </form>
-<?php
-endif; ?>
+<?php endif; ?>
 {% endraw %}
 
 <div class="secondary-buttons">
