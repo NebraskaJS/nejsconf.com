6a44099f3491e27e7489999dcf3defa555542699
Zach Leatherman <zachleatherman@gmail.com>
2015-02-10 23:45:15 -0600

    email keyboardin’ per matt steele

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ layout: default
 <?php if( $render_form ): ?>
 <form method="POST" action="<?php echo $PHP_SELF; ?>#feedback" id="teaser-subscribe"  class="collapsed">
   <label for="teaser-email" class="a11y-only">Email</label>
-  <input type="text" id="teaser-email" name="email" <?php if(isset($_POST['email'])): ?>value="<?php echo htmlspecialchars($_POST['email']); ?>"<?php endif; ?> placeholder="Your e-mail address">
+  <input type="email" id="teaser-email" name="email" <?php if(isset($_POST['email'])): ?>value="<?php echo htmlspecialchars($_POST['email']); ?>"<?php endif; ?> placeholder="Your e-mail address">
   <button type="submit" class="btn-primary">Get notified <span>when tickets go on sale</span></button>
 </form>
 <?php endif; ?>
