e635dc5f44387c424d5a87d40fc851b4ff5cd031
Zach Leatherman <zachleatherman@gmail.com>
2015-04-30 00:27:01 -0500

    Start of the full website.

--- Gruntfile.js
+++ Gruntfile.js
@@ -219,7 +219,7 @@ module.exports = function(grunt) {
 				tasks: ['grunticon', 'content']
 			},
 			content: {
-				files: ['<%= config.root %>_posts/**/*', '<%= config.root %>_layouts/**/*', '<%= config.root %>license/**/*', '<%= config.root %>index.php', '<%= config.root %>_plugins/**/*', '<%= config.root %>_includes/**/*' ],
+				files: ['<%= config.root %>_posts/**/*', '<%= config.root %>_layouts/**/*', '<%= config.root %>license/**/*', '<%= config.root %>index.*', '<%= config.root %>_plugins/**/*', '<%= config.root %>_includes/**/*' ],
 				tasks: ['content']
 			},
 			config: {
