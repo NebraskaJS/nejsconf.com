a74f327d364affc0e2e00f396ee934fb4d53466b
Zach Leatherman <zachleatherman@gmail.com>
2015-07-07 01:10:59 -0500

    Added hotel and schedule

--- assets/js/defer.js
+++ assets/js/defer.js
@@ -10,6 +10,8 @@
 	}
 
 	$( '#venue-photo' ).html( '<img src="/assets/img/venue.jpg" alt="Panorama of the Zoo Venue">' );
+	$( '#hotel-map' ).html( '<img src="http://maps.googleapis.com/maps/api/staticmap?center=101+S+10th+St+Omaha,+NE+68102&zoom=13&scale=2&size=300x200&maptype=roadmap&format=png&visual_refresh=true&markers=size:mid%7Ccolor:red%7Clabel:1%7C101+S+10th+St+Omaha,+NE+68102" alt="Google Map of Hotel">' );
+	$( '#venue-map' ).html( '<img src="http://maps.googleapis.com/maps/api/staticmap?center=3701+S+10th+St+Omaha,+NE+68107&zoom=13&scale=2&size=300x200&maptype=roadmap&format=png&visual_refresh=true&markers=size:mid%7Ccolor:red%7Clabel:2%7C3701+S+10th+St+Omaha,+NE+68107" alt="Google Map of the Zoo">' );
 
 	// if( "JSON" in window ) {
 	// 	$.get( "/tickets-remaining", function( data ) {
