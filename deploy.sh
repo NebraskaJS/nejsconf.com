ae1dcf447634b8f93cf160ce115739512bf1c070
Zach Leatherman <zachleatherman@gmail.com>
2016-02-08 21:52:19 -0600

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 grunt stage;
-echo "Note: Requires an \'nejsconf\' host in .ssh/config";
+echo "Note: Requires an \'nejsconf2015\' host in .ssh/config";
 # Run this as ./deploy.sh clobber to overwrite remote config with local
 # By default this script will sync the remote config to local before deploy
 if [ "$1" != "clobber" ]; then
 scp nejsconf:/home/public/config.php ./_site/config.php
 fi
-rsync -avz ssh ./_site/ nejsconf:/home/public/
+rsync -avz ssh ./_site/ nejsconf2015:/home/public/
