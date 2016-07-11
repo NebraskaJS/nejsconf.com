2da4dac2295041c5632763d7bae4ad016b6ffd8c
John Hobbs <john@velvetcache.org>
2016-07-10 21:22:38 -0500

    Better deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
+set -o errexit
+set -o xtrace
 # Run this as ./deploy.sh clobber to overwrite remote config with local
 # By default this script will sync the remote config to local before deploy
+
+jekyll clean
+gulp
+
 if [ "$1" != "clobber" ]; then
-	echo "# Saving config"
-	scp nejsconf.com:/home/public/register/config.php ./_site/register/config.php
+	rsync -avz --exclude config.php --exclude vendor ./_site/ nejsconf.com:/home/public/
+else
+	rsync -avz --exclude vendor ./_site/ nejsconf.com:/home/public/
 fi
-echo "# Syncing Files"
-rsync -avz ssh ./_site/ nejsconf.com:/home/public/
-echo "# Updating Composer"
 ssh nejsconf.com 'cd /home/public/register && /usr/local/bin/php composer.phar install'
