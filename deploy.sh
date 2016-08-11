d49f7d05727074e9014abbfae37748d7e0d62d12
John Hobbs <john@velvetcache.org>
2016-08-11 11:17:53 -0500

    Make rsync less agressive on deploy.

--- deploy.sh
+++ deploy.sh
@@ -14,8 +14,9 @@ find _site/ -name '*.html' -o -name '*.css' \
          -o -name '.png'   -o -name '.svg' \
      | xargs -I {} sh -c "$COMPRESSOR {} -c > {}.zgz"
 
-rsync -avz --exclude config.php --exclude vendor ./_site/ nejsconf.com:/home/public/
+rsync -rvz --exclude config.php --exclude vendor ./_site/ nejsconf.com:/home/public/
 ssh nejsconf.com 'cd /home/public/register && /usr/local/bin/php composer.phar install'
+
 
 if [ "$NEJS_DEPLOY_HOOK" != "" ]; then
 	curl -X POST --data-urlencode 'payload={"username":"DeployBot","icon_emoji":":ghost:","text":"><https://nejsconf.com|nejsconf.com> deployed!"}' $NEJS_DEPLOY_HOOK
